0ad051ca383638d202d39f7b61f5ee2c0168f91d
ngumamutundu <112886486+ngumamutundu@users.noreply.github.com>
2026-05-22 13:04:34 +0000

    all commits for this chatbot

---
+++ chainlit.md
@@ -0,0 +1,10 @@
+# Welcome to Pio AI Companion! 🚀🤖
+
+Hi 👋 We're excited to have you on board. Pio AI Companion is a powerful tool designed to help you quickly get answers to your questions.
+
+## Useful Links 🔗
+
+- **We have a very nice relaxing channel on youtube and this is the link: https://youtube.com/channel/UCn7tMZEY2LrqMLey08oqSug 📚
+- **Welcome to Realax with Pio real Ai companion, we help you answer any form of question in less than 1 minute.
+- **Give it a like subscribe and share as you continue interacting with our AI platform. 💬
+
---
+++ chat.py
@@ -0,0 +1,41 @@
+from typing import List
+import chainlit as cl
+
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)   
+
+
+def get_prompt(instruction: str, history:List[str] =None) ->str:
+    system ="You are an AI assistant that gives helpful answers.You answer the questions in a short and concise"
+    prompt = f"### System:\n{system}\n\n### User:\n"
+    if len(history) > 0:
+        prompt += f"This is the conversation history:{''.join(history)}.Now answer the question:"
+
+    prompt += f"{instruction}\n\n### Response:\n"
+    return prompt 
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    message_history =cl.user_session.get("message_history")
+    msg = cl.Message(content="")
+    await msg.send()
+
+    prompt = get_prompt(message.content, message_history)
+    response = ""
+    for word in llm(prompt, stream=True):
+        await msg.stream_token(word)
+        response += word
+    await msg.update()
+    message_history.append(response)    
+   
+
+@cl.on_chat_start
+def on_chat_start():
+    cl.user_session.set("message_history", [])
+    global llm
+    llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)   
